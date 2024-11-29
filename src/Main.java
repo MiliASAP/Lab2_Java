52a389fd6080a87ede5b2a8901cdd9afd7d7daf2
MiliASAP <michal.bohun22@gmail.com>
2024-11-29 14:34:17 +0100

    commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Color color = new Color(123,123,123);
+        Color color2 = new Color(123,123,123,123);
+
+        System.out.println(color);
+        System.out.println(color2);
+
+        System.out.println(color.toString());
+        System.out.println(color2.toString());
+
+    }
+}
